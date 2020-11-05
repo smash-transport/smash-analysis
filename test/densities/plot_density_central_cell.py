7c4cfcbf86214fe7bd136292e1f96e9ae7cbbb24
Jan Hammelmann <jan.hammelmann@live.de>
2020-11-05 13:35:24 +0100

    Compare central cell calculation to previous result

--- test/densities/plot_density_central_cell.py
+++ test/densities/plot_density_central_cell.py
@@ -24,6 +24,11 @@ PathToData = sys.argv[1]
 Config = sys.argv[2]
 DataOutfile = sys.argv[3]
 PlotName = sys.argv[4]
+try:
+  comp_prev_version = sys.argv[5]
+  comp_prev_version = True
+except IndexError:
+  comp_prev_version = False
 
 with open(Config, 'r') as f: config_file = yaml.safe_load(f)
 TimeSteps = config_file['Output']['Output_Interval']
@@ -32,6 +37,7 @@ Energy = config_file['Modi']['Collider']['E_Kin']
 StartTime = -3 # fm
 TimeSteps = np.arange(StartTime, FinalTime, TimeSteps)
 smash_analysis_version = sb.analysis_version_string()
+setup ='AuAu_central_cell'
 
 with open(PathToData + '/0/thermodynamics.dat') as f:
     smash_version = f.readline().split(' ')[1]
@@ -57,11 +63,16 @@ for dir in dirs:
 dens_arr = np.asarray(dens_list)
 dens_arr = dens_arr/(n_events*0.16)  # density in units of ground state density
 
-header = 'AuAu @ Ekin = {}AGeV\nsmash and smash analysis version\n{} {}\ntime[fm]  rho/rho_0'.format(Energy, smash_version, smash_analysis_version)
-np.savetxt(DataOutfile, np.c_[TimeSteps, dens_arr], header=header)
+header = '# AuAu @ Ekin = {}AGeV\n# smash and smash analysis version\n# {} {}\n# time[fm]  rho/rho_0\n x    y'.format(Energy, smash_version, smash_analysis_version)
+np.savetxt(DataOutfile, np.c_[TimeSteps, dens_arr], header=header, comments='')
+
+if comp_prev_version:
+    import comp_to_prev_version as cpv
+    processes = []
+    cpv.plot_previous_results('densities', setup, '/' + DataOutfile.split('/')[-1])
 
 # old version ?
-plt.plot(TimeSteps, dens_arr, linewidth=2, linestyle="--")
+plt.plot(TimeSteps, dens_arr, linewidth=2, linestyle="-", label='AuAu')
 plt.text(0.3, 4.5, " SMASH code:      %s\n SMASH analysis: %s\n" % \
             (smash_version, smash_analysis_version), \
             color = "gray", fontsize = 7)
