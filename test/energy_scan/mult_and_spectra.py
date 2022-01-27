Fix wrong order of commands

test/energy_scan/mult_and_spectra.py
@@ -119,9 +119,9 @@ class BulkObservables:
             cos2phi_hist = np.histogram(pT[pdg_and_y_cut], bins = self.ptbins, weights = cos2phi[pdg_and_y_cut])[0]
             self.v2[i,:] += np.where(pt_hist_with_cuts > 0.0, cos2phi_hist, 0.0)
 
-            if (added_total_mult_charha == 0): continue
             pdg_and_y_cut_charha = np.logical_and(ycut, pdgcut_charha)
             added_midrap_yield_charha = pdg_and_y_cut_charha.sum()
+            if (added_total_mult_charha == 0): continue
             self.midrapidity_yield_charha += added_midrap_yield_charha
             b = float(added_midrap_yield_charha) / self.midrapidity_yield_charha
             self.meanpt_midrapidity_charha=self.updated_mean(self.meanpt_midrapidity_charha,pT[pdg_and_y_cut_charha].mean(),b)