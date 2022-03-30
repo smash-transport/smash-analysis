8a8ec5de21701901eb5e85cd884a6f8392212af3
Niklas Götz <goetz@fias.uni-frankfurt.de>
2022-03-30 19:10:30 +0200

    Download sampled particle lists externally

--- submit_to_slurm.sh
+++ submit_to_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ "$#" -ne 5 ]] && [[ "$#" -ne 6 ]]; then
+if [[ "$#" -ne 5 ]] && [[ "$#" -ne 6 ]] && [[ "$#" -ne 7 ]]; then
     echo "Submit SLURM job for given target."
     echo
-    echo "Usage: $0 SMASH_SRC_DIR EIGEN_SRC_DIR GSL_DIR PYTHIA_DIR TARGET [OUTPUT_DIR]"
+    echo "Usage: $0 SMASH_SRC_DIR EIGEN_SRC_DIR GSL_DIR PYTHIA_DIR TARGET [OUTPUT_DIR] [SAMPLED_LISTS]"
     exit 1
 fi
 
@@ -11,12 +11,18 @@ path=$(readlink -f $0)
 root=$(dirname $path)
 
 target=$5
+opt_argument=$6
 
-if [ "$#" -eq 4 ]; then
+if [[ "$#" -eq 5 ]]; then
     dt=`date +%Y_%m_%d_%H:%M:%S`
     output=$root/slurm_results_${dt}_${target}
+elif [[ "$#" -eq 6 ]] && [[ "${opt_argument:0:4}" = "http" ]]; then
+    dt=`date +%Y_%m_%d_%H:%M:%S`
+    output=$root/slurm_results_${dt}_${target}
+    sampled_lists=$6
 else
     output=$6
+    sampled_lists=$7
 fi
 
 mkdir -p $output
@@ -32,7 +38,7 @@ then
   sbatch --job-name=smash-e_scan $root/slurm-job.sh energy_scan $1 $2 $3 $4 $root $output/energy_scan
   sbatch --job-name=smash-FOPI $root/slurm-job.sh FOPI_pions $1 $2 $3 $4 $root $output/FOPI_pions
   sbatch --job-name=smash-dens $root/slurm-job.sh densities $1 $2 $3 $4 $root $output/densities
-  sbatch --job-name=smash-aburner $root/slurm-job.sh afterburner $1 $2 $3 $4 $root $output/afterburner
+  sbatch --job-name=smash-aburner $root/slurm-job.sh afterburner $1 $2 $3 $4 $root $output/afterburner $sampled_lists
 else
   sbatch --job-name=smash-$target $root/slurm-job.sh $target $1 $2 $3 $4 $root $output
 fi
