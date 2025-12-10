a8eb319604e9ebdb22820ce23bb538340e1b4953
Carl <rosenkvist@itp.uni-frankfurt.de>
2025-12-10 11:45:30 +0100

    Changes for running SMASH-3.3 analysis

--- test/report/collect_pdfs_and_configs.sh
+++ test/report/collect_pdfs_and_configs.sh
@@ -8,9 +8,7 @@ if [[ "$#" -ne 2 ]] && [[ "$#" -ne 3 ]]; then
 fi
 
 smash_version=$1
-
-# directory in where all files of the SMASH-analysis runs are located
-results=$2
+results=$2     # e.g. /lustre/.../smash-analysis-devel
 
 # directory where all PDFs and corresponding configs are stored
 if [ "$#" -eq 3 ]; then
@@ -19,93 +17,171 @@ else
   collection_dir="${results}/PDFs_configs_${smash_version}"
 fi
 
-if [ ! -d $collection_dir ]; then
-    mkdir ${collection_dir}
+# prepare collection directory
+if [ ! -d "$collection_dir" ]; then
+    mkdir -p "$collection_dir"
 else
-    rm -r ${collection_dir}
-    mkdir ${collection_dir}
+    rm -rf -- "$collection_dir"
+    mkdir -p "$collection_dir"
     echo "WARNING: Directory to collect PDFs and configs already existed, it is being overwritten."
 fi
+
+# create top-level category dirs (this is what you want to see in ls)
+mkdir -p "${collection_dir}/FOPI_pions"
+mkdir -p "${collection_dir}/afterburner"
+mkdir -p "${collection_dir}/angular_distributions"
+mkdir -p "${collection_dir}/cross_sections"
+mkdir -p "${collection_dir}/densities"
+mkdir -p "${collection_dir}/detailed_balance"
+mkdir -p "${collection_dir}/dileptons"
+mkdir -p "${collection_dir}/elastic_box"
+mkdir -p "${collection_dir}/energy_scan"
 
 echo ""
 echo "Copying pdf files now ..."
 
-# create new subdirectories for energy scan (files are being reordered):
-mkdir ${collection_dir}/energy_scan
-mkdir ${collection_dir}/energy_scan/mtspectra
-mkdir ${collection_dir}/energy_scan/yspectra
-mkdir ${collection_dir}/energy_scan/meanmt
-mkdir ${collection_dir}/energy_scan/meanpt
-mkdir ${collection_dir}/energy_scan/total_multiplicity
-mkdir ${collection_dir}/energy_scan/midrapidity_yield
+########################
+#        PDFs          #
+########################
 
-# create new subdirectories for afterburner run (files are being reordered):
-mkdir ${collection_dir}/afterburner
-mkdir ${collection_dir}/afterburner/mtspectra
-mkdir ${collection_dir}/afterburner/yspectra
-mkdir ${collection_dir}/afterburner/meanmt
-mkdir ${collection_dir}/afterburner/meanpt
-mkdir ${collection_dir}/afterburner/total_multiplicity
-mkdir ${collection_dir}/afterburner/midrapidity_yield
-mkdir ${collection_dir}/afterburner/LHC
-mkdir ${collection_dir}/afterburner/RHIC
+# angular_distributions
+cd "${results}/angular_distributions/test/angular_distributions"
+find . -maxdepth 3 -name "*.pdf" \
+  | xargs -r cp --parents -t "${collection_dir}/angular_distributions"
 
-# copy all PDF files necessary to generate the wiki page
-# folder structure is preserved (except for energy scan, where we reorder)
-cd ${results}/angular_distributions/test
-find angular_distributions -maxdepth 3 -name "*.pdf" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/cross_sections/test
-find cross_sections -maxdepth 3 -name "*.pdf" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/detailed_balance/test
-find detailed_balance -maxdepth 3 -name "*.pdf" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/dileptons/test
-find dileptons -maxdepth 3 -name "plot_mass.pdf" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/elastic_box/test
-find elastic_box -maxdepth 2 -name "*.pdf" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/FOPI_pions/test
-find FOPI_pions -maxdepth 2 -name "*.pdf" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/energy_scan/test
-find energy_scan -maxdepth 2 -name "yspectra*.pdf" | xargs cp -t ${collection_dir}/energy_scan/yspectra/
-find energy_scan -maxdepth 2 -name "mtspectra*.pdf" | xargs cp -t ${collection_dir}/energy_scan/mtspectra/
-find energy_scan -maxdepth 2 -name "meanmt*.pdf" | xargs cp -t ${collection_dir}/energy_scan/meanmt/
-find energy_scan -maxdepth 2 -name "meanpt*.pdf" | xargs cp -t ${collection_dir}/energy_scan/meanpt/
-find energy_scan -maxdepth 2 -name "midrapidity_yield*.pdf" | xargs cp -t ${collection_dir}/energy_scan/midrapidity_yield/
-find energy_scan -maxdepth 2 -name "total_multiplicity*.pdf" | xargs cp -t ${collection_dir}/energy_scan/total_multiplicity/
-cd ${results}/afterburner/test
-find afterburner -maxdepth 2 -name "yspectra*.pdf" | xargs cp -t ${collection_dir}/afterburner/yspectra/
-find afterburner -maxdepth 2 -name "mtspectra*.pdf" | xargs cp -t ${collection_dir}/afterburner/mtspectra/
-find afterburner -maxdepth 2 -name "meanmt*.pdf" | xargs cp -t ${collection_dir}/afterburner/meanmt/
-find afterburner -maxdepth 2 -name "meanpt*.pdf" | xargs cp -t ${collection_dir}/afterburner/meanpt/
-find afterburner -maxdepth 2 -name "midrapidity_yield*.pdf" | xargs cp -t ${collection_dir}/afterburner/midrapidity_yield/
-find afterburner -maxdepth 2 -name "total_multiplicity*.pdf" | xargs cp -t ${collection_dir}/afterburner/total_multiplicity/
-cd ${results}/densities/test
-find densities -maxdepth 2 -name "*.pdf" | xargs cp --parents -t ${collection_dir}/
+# cross_sections
+cd "${results}/cross_sections/test/cross_sections"
+find . -maxdepth 3 -name "*.pdf" \
+  | xargs -r cp --parents -t "${collection_dir}/cross_sections"
 
+# detailed_balance
+cd "${results}/detailed_balance/test/detailed_balance"
+find . -maxdepth 3 -name "*.pdf" \
+  | xargs -r cp --parents -t "${collection_dir}/detailed_balance"
+
+# dileptons
+cd "${results}/dileptons/test/dileptons"
+find . -maxdepth 3 -name "plot_mass.pdf" \
+  | xargs -r cp --parents -t "${collection_dir}/dileptons"
+
+# elastic_box
+cd "${results}/elastic_box/test/elastic_box"
+find . -maxdepth 2 -name "*.pdf" \
+  | xargs -r cp --parents -t "${collection_dir}/elastic_box"
+
+# FOPI_pions
+cd "${results}/FOPI_pions/test/FOPI_pions"
+find . -maxdepth 2 -name "*.pdf" \
+  | xargs -r cp --parents -t "${collection_dir}/FOPI_pions"
+
+# ENERGY SCAN PDFs – reordered into subfolders
+mkdir -p "${collection_dir}/energy_scan/mtspectra"
+mkdir -p "${collection_dir}/energy_scan/yspectra"
+mkdir -p "${collection_dir}/energy_scan/meanmt"
+mkdir -p "${collection_dir}/energy_scan/meanpt"
+mkdir -p "${collection_dir}/energy_scan/total_multiplicity"
+mkdir -p "${collection_dir}/energy_scan/midrapidity_yield"
+
+cd "${results}/energy_scan/test/energy_scan"
+find . -maxdepth 2 -name "yspectra*.pdf" \
+  | xargs -r cp -t "${collection_dir}/energy_scan/yspectra/"
+find . -maxdepth 2 -name "mtspectra*.pdf" \
+  | xargs -r cp -t "${collection_dir}/energy_scan/mtspectra/"
+find . -maxdepth 2 -name "meanmt*.pdf" \
+  | xargs -r cp -t "${collection_dir}/energy_scan/meanmt/"
+find . -maxdepth 2 -name "meanpt*.pdf" \
+  | xargs -r cp -t "${collection_dir}/energy_scan/meanpt/"
+find . -maxdepth 2 -name "midrapidity_yield*.pdf" \
+  | xargs -r cp -t "${collection_dir}/energy_scan/midrapidity_yield/"
+find . -maxdepth 2 -name "total_multiplicity*.pdf" \
+  | xargs -r cp -t "${collection_dir}/energy_scan/total_multiplicity/"
+
+# AFTERBURNER PDFs – reordered into subfolders
+mkdir -p "${collection_dir}/afterburner/mtspectra"
+mkdir -p "${collection_dir}/afterburner/yspectra"
+mkdir -p "${collection_dir}/afterburner/meanmt"
+mkdir -p "${collection_dir}/afterburner/meanpt"
+mkdir -p "${collection_dir}/afterburner/total_multiplicity"
+mkdir -p "${collection_dir}/afterburner/midrapidity_yield"
+mkdir -p "${collection_dir}/afterburner/LHC"
+mkdir -p "${collection_dir}/afterburner/RHIC"
+
+cd "${results}/afterburner/test/afterburner"
+find . -maxdepth 2 -name "yspectra*.pdf" \
+  | xargs -r cp -t "${collection_dir}/afterburner/yspectra/"
+find . -maxdepth 2 -name "mtspectra*.pdf" \
+  | xargs -r cp -t "${collection_dir}/afterburner/mtspectra/"
+find . -maxdepth 2 -name "meanmt*.pdf" \
+  | xargs -r cp -t "${collection_dir}/afterburner/meanmt/"
+find . -maxdepth 2 -name "meanpt*.pdf" \
+  | xargs -r cp -t "${collection_dir}/afterburner/meanpt/"
+find . -maxdepth 2 -name "midrapidity_yield*.pdf" \
+  | xargs -r cp -t "${collection_dir}/afterburner/midrapidity_yield/"
+find . -maxdepth 2 -name "total_multiplicity*.pdf" \
+  | xargs -r cp -t "${collection_dir}/afterburner/total_multiplicity/"
+
+# densities
+cd "${results}/densities/test/densities"
+find . -maxdepth 2 -name "*.pdf" \
+  | xargs -r cp --parents -t "${collection_dir}/densities"
 
 echo "Done copying pdf files."
 echo ""
 echo "Copying config files now ..."
-# copy all config files necessary to generate the wiki page
-# folder structure is preserved (except for energy scan, see above)
-cd ${results}/angular_distributions/test
-find angular_distributions -maxdepth 4 -name "config.yaml" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/cross_sections/test
-find cross_sections -maxdepth 4 -name "config.yaml" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/detailed_balance/test
-find detailed_balance -maxdepth 4 -name "config.yaml" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/dileptons/test
-find dileptons -maxdepth 4 -name "config.yaml" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/elastic_box/test
-find elastic_box -maxdepth 4 -name "config.yaml" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/FOPI_pions/test
-find FOPI_pions -maxdepth 4 -name "config.yaml" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/energy_scan/test
-find energy_scan -maxdepth 5 -name "config.yaml" | xargs cp --parents -t ${collection_dir}/
-cd ${results}/afterburner/test
-find afterburner -maxdepth 5 -path "*/LHC/1/data/config.yaml" | xargs cp -t ${collection_dir}/afterburner/LHC
-find afterburner -maxdepth 5 -path "*/RHIC/1/data/config.yaml" | xargs cp -t ${collection_dir}/afterburner/RHIC
-cd ${results}/densities/test
-find densities -maxdepth 4 -name "config.yaml" | xargs cp --parents -t ${collection_dir}/
+
+########################
+#       CONFIGS        #
+########################
+
+# angular_distributions
+cd "${results}/angular_distributions/test/angular_distributions"
+find . -maxdepth 4 -name "config.yaml" \
+  | xargs -r cp --parents -t "${collection_dir}/angular_distributions"
+
+# cross_sections
+cd "${results}/cross_sections/test/cross_sections"
+find . -maxdepth 4 -name "config.yaml" \
+  | xargs -r cp --parents -t "${collection_dir}/cross_sections"
+
+# detailed_balance
+cd "${results}/detailed_balance/test/detailed_balance"
+find . -maxdepth 4 -name "config.yaml" \
+  | xargs -r cp --parents -t "${collection_dir}/detailed_balance"
+
+# dileptons
+cd "${results}/dileptons/test/dileptons"
+find . -maxdepth 4 -name "config.yaml" \
+  | xargs -r cp --parents -t "${collection_dir}/dileptons"
+
+# elastic_box
+cd "${results}/elastic_box/test/elastic_box"
+find . -maxdepth 4 -name "config.yaml" \
+  | xargs -r cp --parents -t "${collection_dir}/elastic_box"
+
+# FOPI_pions
+cd "${results}/FOPI_pions/test/FOPI_pions"
+find . -maxdepth 4 -name "config.yaml" \
+  | xargs -r cp --parents -t "${collection_dir}/FOPI_pions"
+
+# ENERGY SCAN configs
+cd "${results}/energy_scan/test/energy_scan"
+find . -maxdepth 5 -name "config.yaml" \
+  | xargs -r cp --parents -t "${collection_dir}/energy_scan"
+
+# AFTERBURNER configs (special LHC/RHIC handling)
+cd "${results}/afterburner/test/afterburner"
+find . -maxdepth 5 -path "*/LHC/1/data/config.yaml" \
+  | xargs -r cp -t "${collection_dir}/afterburner/LHC"
+find . -maxdepth 5 -path "*/RHIC/1/data/config.yaml" \
+  | xargs -r cp -t "${collection_dir}/afterburner/RHIC"
+
+# densities
+cd "${results}/densities/test/densities"
+find . -maxdepth 4 -name "config.yaml" \
+  | xargs -r cp --parents -t "${collection_dir}/densities"
+
 echo "Done copying config files."
 echo ""
 echo "Finished copying PDFs and configs."
+echo "Collected into: ${collection_dir}"
+
